ee4c905257034558a775bf9e3b3f02807c9bf646
JeffiProgrammer <fx.ranjbaran@gmail.com>
2022-07-18 08:09:36 +0430

    complete the switch account screen

--- lib/screens/switch_account_screen.dart
+++ lib/screens/switch_account_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class SwitchAccountScreen extends StatelessWidget {
@@ -10,14 +12,71 @@ class SwitchAccountScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/switch_account_background.png"),
-                    fit: BoxFit.fitWidth),
+              child: Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("images/switch_account_background.png"),
+                      fit: BoxFit.fitWidth),
+                ),
               ),
-            ),
-          ),
+              Positioned(
+                top: 170,
+                child: ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+                    child: Container(
+                      width: 270,
+                      height: 289,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color.fromRGBO(255, 255, 255, 0.5),
+                              Color.fromRGBO(255, 255, 255, 0.2),
+                            ]),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      child: Column(children: [
+                        SizedBox(height: 22),
+                        Image(image: AssetImage("images/profile.png")),
+                        SizedBox(height: 10),
+                        Text("Jeffi Programmer",
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                        SizedBox(height: 10),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Confirm",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: Color(0xfff35383),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15.0))),
+                        ),
+                        SizedBox(height: 15),
+                        Text("switch account",
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white)),
+                      ]),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )),
           Padding(
             padding: EdgeInsets.only(top: 50.0, bottom: 10.0),
             child: Text("don't have an account?",
